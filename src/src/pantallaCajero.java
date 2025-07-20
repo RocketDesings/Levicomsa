c07861d7916c306a68ab1e1d8708b5c9b54be136
AmirOrozco11 <amorozcoes@ittepic.edu.mx>
2025-07-20 02:10:10 -0700

    admin

--- src/src/pantallaCajero.java
+++ src/src/pantallaCajero.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 
 public class pantallaCajero {
     private JPanel panelMain;
+    private JLabel lblImgCajero;
+    private JLabel lblLogo;
+    private JTextField txtMontoInicial;
+    private JButton tbnIngresar;
+    private JButton tbnCerrarSesion;
 
 }
 
