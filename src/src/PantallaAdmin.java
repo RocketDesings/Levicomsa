e1656db4bd1bba4ef2e130b2ba8b4d53861ce52e
AmirOrozco11 <amorozcoes@ittepic.edu.mx>
2025-07-20 01:19:27 -0700

    admin

---
+++ src/src/PantallaAdmin.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class PantallaAdmin {
+    private JPanel panelMain;
+    private JLabel lblImagen;
+    private JLabel lblNombre;
+    private JPanel panelInfo;
+    private JLabel lblTitulo;
+    private JLabel lblSlogan;
+    private JLabel lblIcono;
+    private JLabel lblSucursal;
+    private JLabel lblHora;
+    private JTable table1;
+    private JPanel panelBusqueda;
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JPanel panelBotones;
+    private JButton btnAgregarCliente;
+    private JButton btnModificarCliente;
+    private JButton btnCobrar;
+    private JButton btnSalir;
+}
