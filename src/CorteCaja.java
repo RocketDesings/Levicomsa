5852374d0cf3e659bf1413302a9080584c4a60c0
AmirOrozco11 <amorozcoes@ittepic.edu.mx>
2025-10-15 12:10:16 -0700

    INTERFAZ DE EL CORTE DE CAJA

---
+++ src/CorteCaja.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class CorteCaja {
+    private JPanel main;
+    private JLabel lbl1;
+    private JLabel lblSucursal;
+    private JLabel lblFecha;
+    private JLabel lblFondoInicial;
+    private JLabel lblIngresosEfectivo;
+    private JLabel lblIngresosTransferencia;
+    private JLabel lblRetiros;
+    private JTable tblResumen;
+    private JLabel lblEfectivoTeorico;
+    private JLabel lblVentas;
+    private JLabel lblExtras;
+    private JLabel lblIngresosTotales;
+    private JLabel lblDiferencia;
+    private JButton btnContarEfectivoButton;
+    private JButton btnExportarCSV;
+    private JButton btnCerrarCorte;
+    private JLabel lblTotalContadores;
+}
